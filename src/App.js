72c4a96911117d72db416c63151fc47e59285de0
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-07-11 06:25:08 +0530

    get response from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
 import youtube from "./apis/youtube";
+
 const KEY = "AIzaSyDpGsXCfyplwm-Ew3YKwTpqVZxasMExI20";
 
 class App extends React.Component {
-  // state = { videos: [] };
-  onTermSubmit = (term) => {
-    youtube.get("/search", {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
         part: "snippet",
         maxResults: 5,
@@ -14,12 +15,14 @@ class App extends React.Component {
         q: term,
       },
     });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container" style={{ margin: "10px" }}>
-        <SearchBar callWhenSubmitted={this.onTermSubmit} />
+        <SearchBar callWhenSubmitted={this.onTermSubmit} />I have
+        {this.state.videos.length} videos
       </div>
     );
   }
